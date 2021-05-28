a5bd145c99bfbf446a4df7e773f385014ee0eb95
coendokter97 <coendokter97@outlook.com>
2021-05-28 13:36:10 +0200

    Instellingen met click
    .

--- app/src/main/java/com/example/revilt_inf1e/SecondActivity.java
+++ app/src/main/java/com/example/revilt_inf1e/SecondActivity.java
@@ -3,11 +3,30 @@ package com.example.revilt_inf1e;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SecondActivity extends Activity {
+
+    private Button button;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+
+    public void openActivity(){
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/revilt_inf1e/ThirdActivity.java
@@ -0,0 +1,12 @@
+package com.example.revilt_inf1e;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThirdActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
