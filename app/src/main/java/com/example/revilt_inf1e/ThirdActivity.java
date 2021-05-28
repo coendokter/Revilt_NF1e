c5500ca09f49b68d85b3e7b8f123766ce4f73ff6
BatuhanGezgin <batuhan.gezgin@student.nhlstenden.com>
2021-05-28 13:55:40 +0200

    Button settings klaar

--- app/src/main/java/com/example/revilt_inf1e/ThirdActivity.java
+++ app/src/main/java/com/example/revilt_inf1e/ThirdActivity.java
@@ -1,12 +1,30 @@
 package com.example.revilt_inf1e;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends Activity {
+
+    private Button button2;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+
+    public void openActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
