8cbf67c55013b918305dc9a3e594c19f6ab1d4b2
coendokter97 <coendokter97@outlook.com>
2021-06-09 16:04:17 +0200

    connectie met database & api
    Inlog nu mogelijk

--- app/src/main/java/com/example/revilt_inf1e/MainActivity.java
+++ app/src/main/java/com/example/revilt_inf1e/MainActivity.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Handler;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    EditText pas, usr;
+
     //After completion of 2000 ms, the next activity will get started.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        /*new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i=new Intent(MainActivity.this,
@@ -35,6 +41,18 @@ public class MainActivity extends AppCompatActivity {
                 finish();
                 //the current activity will get finished.
             }
-        }, SPLASH_SCREEN_TIME_OUT);
+        }, SPLASH_SCREEN_TIME_OUT);*/
+
+        usr = (EditText) findViewById(R.id.username);
+        pas = (EditText) findViewById(R.id.password);
+    }
+
+    public void loginBtn(View view) {
+        String user = usr.getText().toString();
+        String pass = pas.getText().toString();
+
+        background bg = new background(this);
+        bg.execute(user, pass);
+
     }
 }
